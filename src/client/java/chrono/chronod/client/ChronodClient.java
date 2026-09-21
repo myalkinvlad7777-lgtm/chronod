01312dfce56f742bb691f19f433396c761db9e0c
zowq <myalkinvlad.777.7@gmail.com>
2026-09-21 19:59:57 +0300

    Initial commit

---
+++ src/client/java/chrono/chronod/client/ChronodClient.java
@@ -0,0 +1,10 @@
+package chrono.chronod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChronodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/chrono/chronod/Chronod.java
@@ -0,0 +1,40 @@
+package chrono.chronod;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
+
+public class Chronod implements ModInitializer {
+    public static final String MOD_ID = "chronod";
+
+    public static final Block CHRONO_BLOCK = new Block(AbstractBlock.Settings.create().strength(1,5f));
+
+    public static final RegistryKey<PlacedFeature> CHRONO_SAND_HEAP_PLACED_KEY =
+            RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(MOD_ID, "chrono_sand_heap_placed"));
+
+    @Override
+    public void onInitialize() {
+        Identifier blockId = Identifier.of(MOD_ID, "chronod_block");
+
+        Registry.register(Registries.BLOCK, blockId, CHRONO_BLOCK);
+        Registry.register(Registries.ITEM, blockId, new BlockItem(CHRONO_BLOCK, new Item.Settings()));
+
+        BiomeModifications.addFeature(
+                BiomeSelectors.foundInTheEnd(),
+                GenerationStep.Feature.SURFACE_STRUCTURES,
+                CHRONO_SAND_HEAP_PLACED_KEY
+        );
+    }
+    //d
+}
